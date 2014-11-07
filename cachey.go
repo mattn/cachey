a8cbca2a97ebc3552b7bb7183727a8d90b9ab32e
mattn <mattn.jp@gmail.com>
2014-11-07 12:54:02 +0900

    First import

---
+++ cachey.go
@@ -0,0 +1,95 @@
+package cachey
+
+import (
+	"runtime"
+	"sync"
+	"time"
+)
+
+type item struct {
+	key    string
+	value  interface{}
+	expire time.Time
+}
+
+type cache struct {
+	items map[string]item
+	mutex sync.RWMutex
+	timer *time.Timer
+}
+
+func NewCache() *cache {
+	c := &cache{}
+	c.items = make(map[string]item)
+	c.startGC()
+	return c
+}
+
+func (c *cache) Set(key string, value interface{}, expire time.Duration) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.items[key] = item{
+		value:  value,
+		expire: time.Now().Add(expire),
+	}
+}
+
+func (c *cache) Get(key string) (interface{}, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	item, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+	if item.expire.Before(time.Now()) {
+		delete(c.items, key)
+		return nil, false
+	}
+	return item.value, true
+
+}
+
+func (c *cache) GetOrSet(key string, code func() (interface{}, time.Duration)) (interface{}, bool) {
+	if value, ok := c.Get(key); ok {
+		return value, true
+	}
+	value, expire := code()
+	c.Set(key, value, expire)
+	return value, false
+}
+
+func (c *cache) Delete(key string) (deleted interface{}, ok bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	deleted, ok = c.items[key]
+	delete(c.items, key)
+	return
+}
+
+func (c *cache) startGC() {
+	var t *time.Timer
+	runtime.SetFinalizer(c, func(c *cache) {
+		println(1)
+		t.Stop()
+	})
+	t = time.AfterFunc(1, func() {
+		keys := []string{}
+		c.mutex.RLock()
+		for k := range c.items {
+			keys = append(keys, k)
+		}
+		c.mutex.RUnlock()
+
+		now := time.Now()
+
+		for _, k := range keys {
+			c.mutex.Lock()
+			v := c.items[k]
+			if v.expire.Before(now) {
+				delete(c.items, k)
+			}
+			c.mutex.Unlock()
+		}
+		t.Reset(1)
+	})
+}
---
+++ cachey_test.go
@@ -0,0 +1,121 @@
+package cachey
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestSetAndGet(t *testing.T) {
+	c := NewCache()
+	c.Set("key1", "val1", time.Second)
+	c.Set("key2", "val2", 2 * time.Second)
+	c.Set("key3", "val3", time.Second)
+
+	if v, ok := c.Get("key1"); !ok || v != "val1" {
+		t.Fatal("not set")
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if _, ok := c.Get("key1"); ok {
+		t.Fatal("not expired!")
+	}
+	if _, ok := c.Get("key2"); !ok {
+		t.Fatal("expired!")
+	}
+	if _, ok := c.Get("key3"); ok {
+		t.Fatal("not expired!")
+	}
+}
+
+func TestValueIsNotFound(t *testing.T) {
+	c := NewCache()
+	if v, ok := c.Get("not-found"); v != nil || ok {
+		t.Fatal("found 'not-found-key's cache.")
+	}
+}
+
+func TestGetOrSet(t *testing.T) {
+	c := NewCache()
+	key := "get_or_set"
+	if v, ok := c.Get(key); v != nil || ok {
+		t.Fatal("found 'get_or_set's cache.")
+	}
+
+	value, isGot := c.GetOrSet(key, func() (interface{}, time.Duration) {
+		return "value1", time.Second
+	})
+	if value != "value1" || isGot {
+		t.Fatal("not set")
+	}
+
+	value, isGot = c.GetOrSet(key, func() (interface{}, time.Duration) {
+		return "value2", time.Second
+	})
+	if value == "value2" || !isGot {
+		t.Fatal("set value2")
+	}
+
+	time.Sleep(1 * time.Second)
+
+	// expired. so, not found cache.
+	if value, ok := c.Get(key); value != nil || ok {
+		t.Fatal("not expired")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := NewCache()
+	value, _ := c.GetOrSet("name", func() (interface{}, time.Duration) {
+		return "mattn", 10 * time.Second
+	})
+	if value == "" {
+		t.Fatal("not set")
+	}
+
+	if deletedValue, ok := c.Delete("name"); deletedValue == "" || !ok {
+		t.Fatal("not delete")
+	}
+}
+
+func TestLock(t *testing.T) {
+	c := NewCache()
+	c.Set("name", "mattn", 10*time.Second)
+
+	ch := make(chan string)
+	go func() {
+		c.Get("name")
+		ch <- "1"
+	}()
+	go func() {
+		c.Get("name")
+		ch <- "2"
+	}()
+	go func() {
+		c.Set("name", "mattn", 10*time.Second)
+		ch <- "3"
+	}()
+	go func() {
+		c.Get("name")
+		ch <- "4"
+	}()
+	go func() {
+		c.Delete("name")
+		ch <- "5"
+	}()
+	go func() {
+		c.Get("name")
+		ch <- "6"
+	}()
+
+	var buffer bytes.Buffer
+	for i := 0; i < 6; i++ {
+		buffer.WriteString(<-ch)
+	}
+	if buffer.String() != "123456" {
+		t.Fatal("not locked", buffer.String())
+	}
+}
+
+// This is based on https://github.com/hisaichi5518/vache
